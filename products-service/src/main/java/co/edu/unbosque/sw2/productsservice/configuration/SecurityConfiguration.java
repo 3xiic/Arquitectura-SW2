e4157dfca47484d76112df4973b3d00fc9200d98
Exiic <122654327+3xiic@users.noreply.github.com>
2024-11-09 23:51:36 -0500

    Add files via upload

---
+++ products-service/src/main/java/co/edu/unbosque/sw2/productsservice/configuration/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package co.edu.unbosque.sw2.productsservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/products/create").hasAuthority("SCOPE_email")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwkSetUri("http://localhost:8080/realms/master/protocol/openid-connect/certs")
+                        )
+                );
+
+        return http.build();
+    }
+}
